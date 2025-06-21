f0e897b26f5e8c790562c21bf0cbf4a2e6a66870
Nils <32747859+schoeneberg@users.noreply.github.com>
2025-06-21 10:13:06 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,11 +17,3 @@ extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
